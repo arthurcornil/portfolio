5dc83443fa09a9100cf86a143c608f908138b98f
arthurcornil <102752044+arthurcornil@users.noreply.github.com>
2025-01-22 19:18:13 +0100

    implemented hamburger menu on mobile

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function animateSticker() {
         sticker.classList.remove("spin");
     }, { once: true });
 }
+
+function triggerMenu() {
+    const menu = document.querySelector('#mobileMenu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+        return ;
+    }
+    menu.classList.add('hidden');
+}
